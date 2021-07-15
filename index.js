5879fd5db3bfcca917c25d1a8e59ac4117faa83f
cezin17 <cezarjardim31@gmail.com>
2021-07-15 00:29:11 -0300

    update

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-var btn = document.querySelector('button')
-let max = prompt("Enter the maximum number")
-function randomNumber(random){
-    random = Math.floor(Math.random()*max)
-    console.log(random)
-}
+const btn = document.querySelector('button')
+const input = document.querySelector('input')
 
 btn.onclick = function() {
-    randomNumber()
+    max = input.value
+    randomNumber(max)
 }
+
+input.onchange = function() {
+    if(input.value > 9999999 || input.value < 1){
+        input.value = 20
+        document.getElementById("notify").classList.remove('hidden')
+        setTimeout(()=>document.getElementById("notify").classList.add('hidden'), 5000)
+    }
+}
+function randomNumber(max){
+    const random = Math.floor(Math.random()*max + 1)
+    let time = 1
+    for (let index = 1; index <= random; index += Math.floor(random * 0.2)) {
+        setTimeout(()=> document.getElementById('result').innerHTML = index, time++ * 50)
+    }
+    setTimeout(()=> document.getElementById('result').innerHTML = random, time++ * 50)
+}
+
+
